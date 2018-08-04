de6c24032dbadba9eca949d16915cd372f19e144
maeda1150 <masaki19770108@gmail.com>
2018-08-04 20:31:14 +0900

    get todo list from api
    On branch master
    Changes to be committed:
    modified:   src/components/TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { index } from '../api';
 
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [
-        {
-          key: 1,
-          title: 'Todo1のタイトル',
-          text: 'Todo1のテキスト',
-          created_at: 'Todo1の作成日時',
-        },
-        {
-          key: 2,
-          title: 'Todo2のタイトル',
-          text: 'Todo2のテキスト',
-          created_at: 'Todo2の作成日時',
-        },
-      ]
+      todos: []
     }
+  }
+
+  componentDidMount() {
+    // Todos を取得して todos ステートにセットする
+    index().then(response => this.setState({ todos: response }));
   }
 
   render() {
@@ -37,7 +30,7 @@ class TodoList extends React.Component {
           <tbody>
             {this.state.todos.map(todo => (
               <TodoItem
-                key={todo.key}
+                key={todo.id}
                 title={todo.title}
                 text={todo.text}
                 created_at={todo.created_at}
