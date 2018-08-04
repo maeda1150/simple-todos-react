57d3cd549d6c5f3cba73f11aac73638acd409d16
maeda1150 <masaki19770108@gmail.com>
2018-08-04 20:12:54 +0900

    add todo cards
    On branch master
    Changes to be committed:
    modified:   src/App.js
    new file:   src/components/TodoItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { index, create, destroy } from './api';
+import TodoItem from './components/TodoItem';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <h1>Hello World</h1>
-        {/* onClick prop に index を実行する関数を渡す */}
-        {/* onClick に渡す関数のことをイベントハンドラと呼んだりします */}
-        <button onClick={() => index().then(json => console.log(json))}> {/* index 関数が返した json をコンソールに表示する */}
-          GET /todos
-        </button>
-        <button onClick={() => create('first todo', 'do it').then(json => console.log(json))}>
-          POST /todos
-        </button>
-        <button
-          onClick={() => destroy('5d057940-c4cc-48f7-aa6b-f7a508b9f9d4').then(json => console.log(json))}
-        >
-          DELETE /todos
-        </button>
+        <h3>Todo List</h3>
+        {/* Props を使うことで外から値が渡せる（関数の引数に似ている） */}
+        <TodoItem
+          title="Todo1のタイトル"
+          text="Todo1のテキスト"
+          created_at="Todo1の作成日時"
+        />
+        <TodoItem
+          title="Todo2のタイトル"
+          text="Todo2のテキスト"
+          created_at="Todo2の作成日時"
+        />
       </div>
-    );
+    )
   }
 }
 
